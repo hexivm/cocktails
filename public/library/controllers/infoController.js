8d5a74c582981e0a9eef59746a21cf3afe511e3d
juanccoellop <juanc.coellop@gmail.com>
2015-06-26 16:58:12 +0200

    fix
    fix

--- public/library/controllers/infoController.js
+++ public/library/controllers/infoController.js
@@ -3,6 +3,8 @@ app.controller("infoController", function infoController($scope,$routeParams){
 	angular.forEach($scope.cocktails, function(cocktail, key){
 		if (cocktail._id == $routeParams.id){
 			$scope.cocktail = cocktail;
+			$scope.cocktail.alcoholMessage = cocktail.alcohol ? "Alcoholic" : "Non-Alcoholic";
 		}
+
 	});
 });
